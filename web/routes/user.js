27f491db9b6ca2d4ff5059d5a2933b785febf9a4
Shreyansh Murarka <shreyanshmurarka97@gmail.com>
2019-01-21 00:28:34 +0530

    Fixed routes

--- web/routes/user.js
+++ web/routes/user.js
@@ -13,19 +13,30 @@ const writeconfig = async id => {
     try {
       let currDom = await domain.findOne({ where: {id} });
       let servers = await server.findAll({ where: {domainId: id}});
+
+      let flag = true; // flag to check if any servers are available for this domain
+      for(let i=0;i<servers.length;i++) {
+        if(servers[i].backup == '0' && servers[i].down == '0') {
+          flag = false;
+          break;
+        }
+      }
+      if(flag) {
+        await deleteConfig(id);
+        return resolve();
+      }
       let config = 
-`${currDom.protocol} {
-  upstream myapp${currDom.id} {
-    ${currDom.type == 2?'least_conn;':(currDom.type == 3?'ip_hash;':'')}
-    ${servers.map(x=>(`server ${x.ip} weight=${x.weight};`)).join('\n    ')}
-  }
+`upstream myapp${currDom.id} {
+  ${currDom.type == 2?'least_conn;':(currDom.type == 3?'ip_hash;':'')}
+  ${servers.map(x=>(`server ${x.ip} weight=${x.weight} max_conns=${x.max_conns} max_fails=${x.max_fails} fail_timeout=${x.fail_timeout} ${x.backup==1?'backup':''} ${x.down==1?'down':''};`))
+  .join('\n    ')}
+}
 
-  server {
-    listen ${currDom.protocol == 'http'?'80':'443'};
+server {
+  listen ${currDom.protocol == 'http'?'80':'443'};
 
-    location / {
-      proxy_pass ${currDom.protocol}://myapp${currDom.id};
-    }
+  location / {
+    proxy_pass ${currDom.protocol}://myapp${currDom.id};
   }
 }
 `
@@ -40,13 +51,31 @@ const writeconfig = async id => {
   });
 }
 
+const deleteConfig = async id =>{
+  return new Promise(async(resolve,reject) =>{
+    try {
+      let currDom = await domain.findOne({ where: { id }});
+      fs.unlink(path.join(__dirname,'..','..','conf',currDom.protocol+'.'+currDom.name+'.nginx.conf'),err=>{
+        resolve();
+      });
+    } catch(err) {
+      return reject(err);
+    }
+  });
+};
+
+const restartNginx = new Promise(async (resolve,reject) => {
+  //figure this part out
+  return resolve();
+});
+
 router.post(
   '/addDomain',
   validator(schemas.addDomain),
   async (req,res) => {
     try {
       let newDomain = await domain.create(req.body);
-      await writeconfig(1);
+      await writeconfig(newDomain.id);
       return res.sendSuccess(newDomain);
     } catch (err) {
       return res.sendError(err);
@@ -59,6 +88,7 @@ router.post(
   validator(schemas.deleteDomain),
   async (req,res) => {
     try {
+      await deleteConfig(req.body.id);
       await server.destroy({ where: {domainId: req.body.id } });
       await domain.destroy({ where: { id: req.body.id } });
       return res.sendSuccess();
